df51a2da669a49ca9532529655b1d05b1c5ad7b2
Shlomi <vgtvgy1@gmail.com>
2020-09-17 15:13:45 +0300

    Update ghidra.sh

--- ghidra.sh
+++ ghidra.sh
@@ -14,28 +14,6 @@ curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install gradle 5.0
 
-#sudo apt install flex bison
-
-
-#dex2jar
-#wget -c --no-clobber https://github.com/pxb1988/dex2jar/releases/download/2.0/dex-tools-2.0.zip 
-#unzip dex-tools-2.0.zip
-
-#AXMLPrinter2 
-#wget -c --no-clobber https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/android4me/AXMLPrinter2.jar
-
-#HFS Explorer version 23.1 (newer than 21)
-#wget -c --no-clobber https://github.com/unsound/hfsexplorer/releases/download/hfsexplorer-0.23.1/hfsexplorer-0.23.1-bin.zip
-
-#Yet Another Java Service Wrapper 12.12
-#wget -c --no-clobber https://sourceforge.net/projects/yajsw/files/yajsw/yajsw-stable-12.12/yajsw-stable-12.12.zip/download
-
-#Eclipse PDE
-
-#Eclipse CDT
-
-#PyDev
-
 #Ghidra
 folder="ghidra"
 url="https://github.com/NationalSecurityAgency/ghidra"
@@ -56,28 +34,3 @@ gradle prepDev
 #Unzip build
 cd build/dist/
 unzip *
-
-#Print location
-dir=$(ls -d */)
-cd $dir
-echo "
-
-
-
-===============================
-===============================
-===============================
-Ghidra is located at:
-"
-pwd
-echo "
-===============================
-===============================
-===============================
-
-
-
-"
-
-
-
